02cca5d49f73a7ce2560c8a4b345c786f0b74dd8
Stephen Oyetoro <oyetorostephven@gmail.com>
2022-05-20 17:42:57 +0200

    Update dns.ps1

--- dns.ps1
+++ dns.ps1
@@ -3,7 +3,8 @@ Set-DnsServerForwarder -IPAddress "168.63.129.16"
 
 Install-WindowsFeature RemoteAccess -IncludeManagementTools
 Install-WindowsFeature -Name Routing -IncludeManagementTools -IncludeAllSubFeature
-Install-WindowsFeature -Name "RSAT-RemoteAccess-Powershell"Install-RemoteAccess -VpnType RoutingOnly
+Install-WindowsFeature -Name "RSAT-RemoteAccess-Powershell"
+Install-RemoteAccess -VpnType RoutingOnly
 Get-NetAdapter | Set-NetIPInterface -Forwarding Enabled
 
 #DownloadSecurityBaseline
